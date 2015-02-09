6dd415c9649839b2485eb317a3b0eb9387f9fd09
Carroll Vance <vancecs@gmail.com>
2015-02-09 04:30:28 -0600

    Remove TB-3 transport / Add VI25 Output support

--- alesis/vi25.control.js
+++ alesis/vi25.control.js
@@ -66,11 +66,9 @@ function onMidi(status, data1, data2) {
 }
 
 function onSysex(data) {
-	//printSysex(data);
 }
 
 function exit() {
-   // nothing to do here ;-)
 }
 
 function init() {
@@ -83,10 +81,13 @@ function init() {
 	// Setting Callbacks for Midi and Sysex
 	host.getMidiInPort(0).setMidiCallback(onMidi);
 	host.getMidiInPort(0).setSysexCallback(onSysex);
+	
+	//Send MIDI Clock
+	host.getMidiOutPort(0).setShouldSendMidiBeatClock(true);
 
 	transport = host.createTransport();
 
-	   // Make CCs 2-119 freely mappable for all 16 Channels
+	// Make CCs 2-119 freely mappable for all 16 Channels
 	userControls = host.createUserControlsSection((HIGHEST_CC - LOWEST_CC + 1)*16);
 
 	for(var i=LOWEST_CC; i<=HIGHEST_CC; i++)
